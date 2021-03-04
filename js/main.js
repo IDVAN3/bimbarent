0b9dec4f5ccf59f761ea0d99f56a9389f11184e0
IDVAN3 <53148453+IDVAN3@users.noreply.github.com>
2021-03-04 19:48:35 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -20,13 +20,13 @@ $(document).ready(function () {
     /* === POPUP === */
 
     let unlock = true;
-      const timeout = 200;
+      const timeout = 300;
       const body = document.querySelector("body");
       const lockPadding = document.querySelectorAll(".lock-padding");
       
       function bodyLock() {
         const lockPaddingValue =
-          window.innerWidth - document.querySelector("body").offsetWidth + "px";
+          window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
         if (lockPadding.length > 0) {
           for (let i = 0; i < lockPadding.length; i++) {
             const el = lockPadding[i];
@@ -34,7 +34,7 @@ $(document).ready(function () {
           }
         }
         body.style.paddingRight = lockPaddingValue;
-        body.classList.add("lock");
+        body.classList.add("lock-scroll");
       
         unlock = false;
         setTimeout(function () {
@@ -49,7 +49,7 @@ $(document).ready(function () {
             el.style.paddingRight = "0px";
           }
           body.style.paddingRight = "0px";
-          body.classList.remove("lock");
+          body.classList.remove("lock-scroll");
         }, timeout);
       
         unlock = false;
@@ -115,7 +115,7 @@ $(document).ready(function () {
     $('.link-show-text').click(function(e) {
       e.preventDefault();
       let allText = $(this).closest('.faq__body-show').find('p');
-      $(this).slideUp(300);
+      $(this).slideUp(0);
       allText.addClass('active');
     })
     /* === /SHOW TEXT === */
@@ -124,7 +124,21 @@ $(document).ready(function () {
     
     $('.header__link').click(function(event){
         event.preventDefault(); 
-        $(this).closest('.header__wrapper-submenu').find('.header__submenu').toggleClass('active');
+        let container = $(this).closest('.header__wrapper-submenu').find('.header__submenu');
+        if(!container.hasClass('active')){
+          $('.header__submenu').removeClass('active');
+          container.addClass('active');
+        }
+        else {
+          container.toggleClass('active');
+        }  
+    });
+
+    $(document).mouseup(function (e) {
+      let container = $('.header__submenu');
+      if (e.target!=container[0] && !container.has(e.target).length && e.target!=$('.header__link')[0] && $('.header__link').has(e.target).length === 0) {
+        container.removeClass('active');
+      }
     });
 
     $('.header__submenu-link').click(function(event){ 
@@ -150,6 +164,180 @@ $(document).ready(function () {
       })
     });
     /* === /SUB MENU === */
+    
+moment.locale('ru');
+    $('input[name="dates"]').daterangepicker({
+      autoUpdateInput: false,
+      minDate: moment(),
+      maxDate: moment().add(365, 'days'),
+      buttonClasses: 'btn-date',
+      locale: {
+        "autoApply": 'true',
+        "format": 'DD-MMMM',
+          "cancelLabel": 'Очистить',
+          "applyLabel": 'Показать <span>105</span> вариантов',
+          "monthNames" : ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+	        "daysOfWeek" : ['вс','пн','вт','ср','чт','пт','сб'],
+          "firstDay": 1,
+      },  
+      
+  });
+
+  /* === SLIDERS === */
+  $('.catalog__filter-slider').slick({
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    arrows: true,
+    infinite: false,
+    variableWidth: true,
+    responsive: [{
+        breakpoint: 1124,
+        settings: {
+            slidesToShow: 3,
+        }
+    },
+    {
+        breakpoint: 992,
+        settings: {
+            slidesToShow: 2,
+        }
+    },
+    {
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 1,
+        }
+    }
+    ]
+})
+
+
+$('.catalog__filter-link').click(function(event) {
+  event.preventDefault();
+  $('.catalog__filter-link').removeClass('active');
+  $(this).addClass('active');
+});
+
+$(".js-range-slider").ionRangeSlider({
+  type: "double", // два инпута
+  min: 300,  // минимальное значение
+  max: 2000, // максимальное значение
+  from: 500, // начальное значение первого 
+  to: 1500, // начальное значение второго
+  hide_min_max: true, // скрыть минимальные и максимальные значения по бокам
+  skin: "none", // скрыть стандартное оформление
+  extra_classes: "my-skin", // класс для своих стилей
+  force_edges: true, // слайдер внутри контейнера
+  drag_interval: true,
+});
+/* === /SLIDERS === */
+
+/* === SELECT === */  
+  let selectBlock = $('.select');
+  let select = function () {
+    let selectHeader = document.querySelectorAll('.select__header');
+    let selectItem = document.querySelectorAll('.select__item');
+
+    selectHeader.forEach(item => {
+        item.addEventListener('click', selectToggle);
+    });
+
+    selectItem.forEach(item => {
+        item.addEventListener('click', selectChoose);
+    });
+
+    function selectToggle() {
+        selectBlock.removeClass('is-active');
+        this.parentElement.classList.add('is-active');
+    }
+
+    function selectChoose() {
+        let text = this.innerText,
+            select = this.closest('.select'),
+            currentText = select.querySelector('.current-text');
+        $(this).closest('.select').find($('.current-text')).addClass('active');
+        currentText.innerText = text;
+        select.classList.remove('is-active');
+        console.log(container.length)
+
+    }
+
+  };
+
+
+  let container = $(".select__body");
+  let header = $(".select__header");
+
+  $(document).mouseup(function (e) {
+    
+    if (container.has(e.target).length === 0 &&  header.has(e.target).length === 0){
+      selectBlock.removeClass('is-active');
+    }       
+  });
+
+  select();
+
+  $('.js-region').click(function(event) {
+    event.preventDefault();
+    $('.select-region').toggleClass('active');
+  })
+  $('.select__item').click(function(event) {
+    event.preventDefault();
+    $('.select__item').removeClass('active');
+    $(this).toggleClass('active');
+  })
+  $(document).mouseup(function (e) {
+    let container = $('.select-region');
+    if (e.target!=container[0] && !container.has(e.target).length && e.target!=$('.js-region')[0] && $('.js-region').has(e.target).length === 0) {
+        container.removeClass('active');
+    }
+});
+  /* === /SELECT === */  
+
+  $(".wrapper-input-number").each( function() {
+    let $quantityArrowMinus = $(this).find(".quantity-arrow-minus");
+    let $quantityArrowPlus = $(this).find(".quantity-arrow-plus");
+    let $quantityNum = $(this).find('.input-number')
+
+    $quantityArrowMinus.click(quantityMinus);
+    $quantityArrowPlus.click(quantityPlus);
+  
+    function quantityMinus() {
+      if ($quantityNum.val() > 0) {
+          $quantityNum.val(+$quantityNum.val() - 1);
+          $quantityArrowPlus.removeClass('stop');
+      }
+      if ($quantityNum.val() == 0) {
+        $quantityArrowMinus.addClass('stop');
+      }
+    }
+  
+    function quantityPlus() {
+      if ($quantityNum.val() < 100) {
+          $quantityNum.val(+$quantityNum.val() + 1);
+          $quantityArrowMinus.removeClass('stop');
+      }
+      if ($quantityNum.val() == 100) {
+        $quantityArrowPlus.addClass('stop');
+      }
+    }
+
+    $quantityNum.change(function(){
+      if (+$(this).attr('max') < $(this).val()) {
+        $(this).val($(this).attr('max'));
+        $quantityArrowPlus.addClass('stop');
+        $quantityArrowMinus.removeClass('stop');
+        
+      }
+      if (+$(this).attr('min') > $(this).val()) {
+        $(this).val($(this).attr('min'));
+        $quantityArrowMinus.addClass('stop');
+        $quantityArrowPlus.removeClass('stop');
+      }
+    });
+  });
+
+  
 
 });
 /*кнопка прокрутки вверх*/
